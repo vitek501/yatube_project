c3c24ac08a6720d9579c38e0e8fc74e9aace5c5d
vitek501 <vitek.kurdyukov@gmail.com>
2022-09-04 19:54:36 +0500

    Добавлены html шаблоны и подключена бд

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.TextField()
+    slug = models.SlugField(unique=True, )
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("group_list", args=[str(self.id)])
+
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+        )
+    group = models.ForeignKey(
+            Group,
+            blank=True,
+            null=True,
+            on_delete=models.CASCADE
+            )
--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'posts'
 
 urlpatterns = [
     path('', views.index),
-    path('group/<slug:slug>/', views.groups_posts),
+    path('group_list/<slug:slug>', views.group_posts, name='group_list'),
 ]
